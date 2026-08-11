deb1511fd83eb4a118d7411f3639bebc8e551bcf
Rupangshu <rupangshu1010@gmail.com>
2026-08-11 20:28:48 +0530

    Update NeuralNetwork.h
    Added memory management functions for the neural network.

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -1,16 +1,38 @@
+/*
+ * ============================================================================
+ * SOFTWARE ARCHITECTURE NOTICE
+ * * Copyright (c) 2026 Rupangshu Manna. All Rights Reserved.
+ *
+ * PROPRIETARY AND CONFIDENTIAL
+ *
+ * This source code, associated data structures, and underlying hardware logic
+ * models represent the intellectual property of Rupangshu Manna.
+ * * Direct bitwise manipulation algorithms, custom memory mapping protocols, 
+ * and underlying network triggering interfaces contained herein may not be 
+ * copied, reverse-engineered, decompiled, modified, or redistributed in any 
+ * form without express written authorization from the copyright holder.
+ * ============================================================================
+ */
+
 #ifndef NEURAL_NETWORK_H
 #define NEURAL_NETWORK_H
 
 #define MaxNode 100000
 
+/* TELEMETRY */
 unsigned char NetGetAlloc(void);
 unsigned char NetGetStats(void);
 unsigned char NetGetLearn(void);
 
+/* CONTROLS */
 unsigned char NetInput(unsigned char Input);
 unsigned char NetTarget(unsigned char Input);
 unsigned char NetAlloc(unsigned int Alloc);
 unsigned char NetInitData(void);
 unsigned char NetTrigg(void);
+
+/* MEMORY TUNNEL */
+unsigned short* NetGetMemory(void);
+unsigned char NetWriteMemory(unsigned int index, unsigned short data);
 
 #endif
